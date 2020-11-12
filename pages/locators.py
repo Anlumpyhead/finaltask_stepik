ef39d2d8c790b37b97f69b7cd3e61ab298d1a454
Surnova Anna <anna.surnova@t-systems.ru>
2020-11-12 13:55:48 +0300

    final changes

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,25 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
+    REG_EMAIL = (By.CSS_SELECTOR, "#register_form #id_registration-email")
+    REG_PASS = (By.CSS_SELECTOR, "#register_form #id_registration-password1")
+    REG_PASS_CONF = (By.CSS_SELECTOR, "#register_form #id_registration-password2")
+    REG_BTN = (By.NAME, "registration_submit")
+    
+class ProductPageLocators():
+    BTN_CARD = (By.CLASS_NAME, "btn-add-to-basket")
+    BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BOOK_TO_CART = (By.CSS_SELECTOR, ".alert-success:first-child .alertinner strong")
+    PRICE_TO_CART = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+    
+class BasketPageLocators():
+    BASKET_LINK = (By.CSS_SELECTOR, "span.btn-group")
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "checkout-quantity")
+    MESSAGE_EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner")
+    
--- pages/login_page.py
+++ pages/login_page.py
@@ -19,3 +19,14 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REG_FORM), "Register form is not presented"
+        
+    def register_new_user(self, email, password):
+        #принимает две строки и регистрирует пользователя
+        reg_email = self.browser.find_element(*LoginPageLocators.REG_EMAIL)
+        reg_email.send_keys(email)
+        reg_pass = self.browser.find_element(*LoginPageLocators.REG_PASS)
+        reg_pass.send_keys(password)
+        conf_pass = self.browser.find_element(*LoginPageLocators.REG_PASS_CONF)
+        conf_pass.send_keys(password)
+        btn_reg = self.browser.find_element(*LoginPageLocators.REG_BTN)
+        btn_reg.click()
